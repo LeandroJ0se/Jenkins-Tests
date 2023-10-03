50dd82f12c4e4309dbe1d8bf4da61bc96df07b02
Leandro José <leandro_daniel08@hotmail.com>
2023-10-03 20:33:09 +0100

    Add project setup

---
+++ src/main/java/lj/project/test/controller/TestApplication.java
@@ -0,0 +1,13 @@
+package lj.project.test.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
